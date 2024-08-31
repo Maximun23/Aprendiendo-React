3fa3837d319c1fa2e94bc58165069de92b898a2c
Maximun23 <maximundilan@gmail.com>
2024-08-30 23:55:52 -0500

    componente seguir x

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,18 @@
+export function App () {
+    return (
+        <article>
+            <header>
+                <img alt="El avatar de midudev" src="https://unavatar.io/maximun12231" />
+                <div>
+                    <strong>Maximun1223YT</strong>
+                    <span>@maximun12231</span>
+                </div>
+            </header>
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
