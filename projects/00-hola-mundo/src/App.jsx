b40b2ef26cde5a78464df5f4fb63d5bd9c59abe6
Maximun23 <maximundilan@gmail.com>
2024-09-01 00:16:21 -0500

    state, useState e intial

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,29 +1,17 @@
+import { useState } from 'react'
 import '../src/App.css'
 import { XFollowCard } from './XFollowCard'
 
 export function App () {
     return (
         <section className='container'>
-            <XFollowCard
-            isFollowing
-            userName={"midudev"}
-            name={"Miguel Ángel Durán"} />
+            <XFollowCard  userName={"midudev"} initialIsFollowing={true}>
+                Miguel Ángel Durán
+            </XFollowCard>
 
-            <XFollowCard
-            isFollowing={false}
-            userName={"Maximun12231"} name={"Maximun1223YT"} />
-
-            <XFollowCard
-            isFollowing
-            userName={"NASA"} name={"NASA"} />
-
-            <XFollowCard
-            isFollowing
-            userName={"Google"} name={"Google"} />
-
-            <XFollowCard    
-            isFollowing
-            userName={"daviardev"} name={"daviardev"} />
+            <XFollowCard  userName={"Maximun12231"}>
+                Maximun1223YT
+            </XFollowCard>
         </section>
     )
 }
--- projects/00-hola-mundo/src/XFollowCard.jsx
+++ projects/00-hola-mundo/src/XFollowCard.jsx
@@ -1,19 +1,34 @@
-export function XFollowCard ({ userName, name, isFollowing}) {
-    console.log(isFollowing)
+import { useState } from 'react'
+
+export function XFollowCard ({ children, userName, initialIsFollowing}) {
+    const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
+
+
+    console.log('[x-followCard] render with userName: ', userName)
+
+    const text = isFollowing ? 'Siguiendo': 'Seguir'
+    const buttonClassname = isFollowing 
+        ? 'x-followCard-button is-following' 
+        : 'x-followCard-button'
+
+    const handleClick = () => {
+        setIsFollowing(!isFollowing)
+    }
 
     return (
         <article className='x-followCard'>
             <header className='x-followCard-header'>
                 <img className='x-followCard-avatar'alt="El avatar de midudev" src={`https://unavatar.io/${userName}`} />
                 <div className='x-followCard-info'>
-                    <strong className='x-followCard-inforUserName-head'>{name}</strong>
+                    <strong className='x-followCard-inforUserName-head'>{children}</strong>
                     <span className='x-followCard-inforUserName'>@{userName}</span>
                 </div>
             </header>
             <aside>
-                <button className='x-followCard-button'>
-                    Seguir
+                <button className={buttonClassname} onClick={handleClick}>
+                    {text}
                 </button>
+                    
             </aside>
             
         </article>
