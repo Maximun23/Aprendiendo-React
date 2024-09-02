d88fecf11be8e0ee1429181b003f50bbc1fdd1ec
Maximun23 <maximundilan@gmail.com>
2024-09-01 20:03:32 -0500

    uwu

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,16 +1,43 @@
 import '../src/App.css'
 import { XFollowCard } from './XFollowCard'
 
+const users = [
+    {
+        userName: 'Maximun12231',
+        name: 'Maximun1223YT',
+        isFollowing: false,
+    },
+    {
+        userName: 'midudev',
+        name: 'Miguel Ángel Durán',
+        isFollowing: false,
+    },
+    {
+        userName: 'petrogustavo',
+        name: 'Gustavo Petro',
+        isFollowing: false,
+    },
+    {
+        userName: 'UmbrellaAcad',
+        name: 'Umbrella Academy',
+        isFollowing: false,
+    },
+]
+
 export function App () {
     return (
         <section className='container'>
-            <XFollowCard  userName={"midudev"} initialIsFollowing={true}>
-                Miguel Ángel Durán
-            </XFollowCard>
-
-            <XFollowCard  userName={"Maximun12231"}>
-                Maximun1223YT
-            </XFollowCard>
+           {
+            users.map(({userName, name, isFollowing }) =>(
+                <XFollowCard 
+                key ={userName}
+                userName={userName}
+                initialIsFollowing={isFollowing}
+                >
+                   {name}
+                </XFollowCard>
+            ))
+           }
         </section>
     )
 }
