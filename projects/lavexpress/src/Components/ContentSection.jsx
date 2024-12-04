5c3369a4e2c8947bb59d366438009029086aedea
Maximun23 <128232838+Maximun23@users.noreply.github.com>
2024-12-04 00:13:25 -0500

    Actualizar ContentSection.jsx

--- projects/lavexpress/src/Components/ContentSection.jsx
+++ projects/lavexpress/src/Components/ContentSection.jsx
@@ -1,4 +1,5 @@
-import ServicePlans from './ServicePlans';
+
+import ServicePlans from './ServicePlans';  // Importa el componente ServicePlans
 
 const ContentSection = ({ id, title, content, active }) => {
   return (
@@ -9,7 +10,10 @@ const ContentSection = ({ id, title, content, active }) => {
       <h2>{title}</h2>
       <p>{content}</p>
 
-      {id === "services" && activate === id && (<ServicePlans />)}
+      {/* Solo mostrar la lista de servicios si la sección está activa */}
+      {id === "services" && active === id && (
+        <ServicePlans />  {/* Aquí se inserta el componente de planes de servicio */}
+      )}
 
       {/* Solo mostrar la lista de localidades si la sección está activa */}
       {id === "locations" && active === id && (
@@ -18,54 +22,7 @@ const ContentSection = ({ id, title, content, active }) => {
           <li>Bellavista</li>
           <li>Riomar</li>
           <li>San Felipe</li>
-          <li>Los Andes</li>
-          <li>Altamira</li>
-          <li>Miramar</li>
-          <li>Alameda</li>
-          <li>El Valle</li>
-          <li>San Isidro</li>
-          <li>Los Pinos</li>
-          <li>Alameda del Río</li>
-          <li>América</li>
-          <li>Barlovento</li>
-          <li>Barranquillita</li>
-          <li>Barrio Abajo</li>
-          <li>Betania</li>
-          <li>Boston</li>
-          <li>Campo Alegre</li>
-          <li>Centro</li>
-          <li>Ciudad Jardín</li>
-          <li>Colombia</li>
-          <li>El Boliche</li>
-          <li>El Castillo</li>
-          <li>El Porvenir</li>
-          <li>El Recreo</li>
-          <li>El Rosario</li>
-          <li>El Tabor</li>
-          <li>Granadillo</li>
-          <li>Zona Industrial Vía 40</li>
-          <li>La Bendición de Dios</li>
-          <li>La Campiña</li>
-          <li>La Concepción</li>
-          <li>La Cumbre</li>
-          <li>La Felicidad</li>
-          <li>La Loma</li>
-          <li>Las Colinas</li>
-          <li>Las Delicias</li>
-          <li>Las Mercedes</li>
-          <li>Los Alpes</li>
-          <li>Los Jobos</li>
-          <li>Los Nogales</li>
-          <li>Modelo</li>
-          <li>Montecristo</li>
-          <li>Nuevo Horizonte</li>
-          <li>Paraíso</li>
-          <li>Parque Rosado</li>
-          <li>San Francisco</li>
-          <li>Santa Ana</li>
-          <li>Villa Country</li>
-          <li>Villa Tarel</li>
-          <li>Villanueva</li>
+          {/* Aquí sigue la lista de localidades */}
         </ul>
       )}
     </section>
