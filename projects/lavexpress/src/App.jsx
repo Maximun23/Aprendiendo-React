e5d8c8eb5387072afc24d610d0a9926ed1cf605a
Maximun23 <128232838+Maximun23@users.noreply.github.com>
2024-12-04 01:07:34 -0500

    Actualizar App.jsx

--- projects/lavexpress/src/App.jsx
+++ projects/lavexpress/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import "./carousel.css"
 import Header from "./Components/Header";
 import Carousel from "./Components/Carousel";
 import ContentSection from "./Components/ContentSection";
