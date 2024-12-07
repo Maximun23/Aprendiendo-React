432b321a8b004d0a93b52eb2b7ea7a9aea882339
Maximun23 <maximundilan@gmail.com>
2024-12-06 23:33:13 -0500

    error solucionado por ultima vez por favooorrrr

--- projects/lavexpress/src/App.jsx
+++ projects/lavexpress/src/App.jsx
@@ -8,7 +8,7 @@ import Header from "./Components/Header";
 import Carousel from "./Components/Carousel";
 import ContentSection from "./Components/ContentSection";
 import Footer from "./Components/Footer";
-import Menu from "./Components/Prueba.jsx"; // El componente al que rediriges
+import Menu from "./Components/Menu.jsx"; // El componente al que rediriges
 import { useState } from "react";
 
 function App() {
@@ -72,7 +72,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/prueba" element={<Menu />} />
+          <Route path="/menu" element={<Menu />} />
           {/* Agrega más rutas según sea necesario */}
         </Routes>
         <Footer />
